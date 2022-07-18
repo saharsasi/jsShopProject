2c0dbb0fc678e4db622b90f578d57c6c2379ccfb
dvir455 <thedvirr@gmail.com>
2022-07-18 22:49:31 +0300

    js

--- products.js
+++ products.js
@@ -1,123 +1,166 @@
-let colors = [
-    "red",
-    "gray",
-    "blue",
-    "black"
-]
+const colors = ['red', 'gray', 'blue', 'black'];
 const products = [
-    {
-        catagory: 'apple',
-        phones: [{
-            model: "iphone 13",
-            price: 800,
-            colors,
-            productInfo: `Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode`,
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "iphone 13 pro max",
-            price: 1200,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "iphone 12",
-            price: 700,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "iphone 12 pro",
-            price: 850,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    },
-{
-        catagory: 'samsung',
-        phones: [{
-            model: "galaxy S21",
-            price: 600,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "galaxy S21 Plus",
-            price: 750,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "galaxy S22 ",
-            price: 800,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "galaxy S22 ultra",
-            price: 900,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    },
-{
-        catagory: 'xiaomi',
-        phones: [{
-            model: "redmi note10",
-            price: 600,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "redmi note10 pro",
-            price: 700,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "xiaomi 12",
-            price: 500,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "xiaomi 12 pro",
-            price: 580,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    },
-{
-        catagory: 'huawei',
-        phones: [{
-            model: "p30",
-            price: 400,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "p30 pro",
-            price: 500,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "p40",
-            price: 600,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "p40 pro",
-            price: 750,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    }]
+  {
+    catagory: 'apple',
+    phones: [
+      {
+        id: 'iphone13',
+        model: 'iphone 13',
+        price: 800,
+        colors,
+        productInfo: `2Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode`,
+        imgUrl:
+          'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg',
+      },
+      {
+        id: 'iphone13promax',
+        model: 'iphone 13 pro max',
+        price: 1200,
+        colors,
+        productInfo:
+          '1Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png',
+      },
+      {
+        id: 'iphone12',
+        model: 'iphone 12',
+        price: 700,
+        colors,
+        productInfo:
+          '4Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl: 'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png',
+      },
+      {
+        id: 'iphone12pro',
+        model: 'iphone 12 pro',
+        price: 850,
+        colors,
+        productInfo:
+          '6Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000',
+      },
+    ],
+  },
+  {
+    catagory: 'samsung',
+    phones: [
+      {
+        model: 'galaxy S21',
+        price: 600,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg',
+      },
+      {
+        model: 'galaxy S21 Plus',
+        price: 750,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png',
+      },
+      {
+        model: 'galaxy S22 ',
+        price: 800,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl: 'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png',
+      },
+      {
+        model: 'galaxy S22 ultra',
+        price: 900,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000',
+      },
+    ],
+  },
+  {
+    catagory: 'xiaomi',
+    phones: [
+      {
+        model: 'redmi note10',
+        price: 600,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg',
+      },
+      {
+        model: 'redmi note10 pro',
+        price: 700,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png',
+      },
+      {
+        model: 'xiaomi 12',
+        price: 500,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl: 'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png',
+      },
+      {
+        model: 'xiaomi 12 pro',
+        price: 580,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000',
+      },
+    ],
+  },
+  {
+    catagory: 'huawei',
+    phones: [
+      {
+        model: 'p30',
+        price: 400,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg',
+      },
+      {
+        model: 'p30 pro',
+        price: 500,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png',
+      },
+      {
+        model: 'p40',
+        price: 600,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl: 'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png',
+      },
+      {
+        model: 'p40 pro',
+        price: 750,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000',
+      },
+    ],
+  },
+];
