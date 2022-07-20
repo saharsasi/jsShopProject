8c24891c49c0492a88c77e8c88730914e0613568
ilaymar95 <ilay.marciano95@gmail.com>
2022-07-20 15:43:44 +0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,124 @@
-"use strict"
-let catagory =''
+'use strict';
+let gSelectedCatagory = '';
 
+function setCatagory(category) {
+  gSelectedCatagory = category; // sets the global selected Category
+  renderCatagoryPage(); // calls the function to render the page
+}
 
+function renderCatagoryPage() {
+  //! Selectors
+  const productContainer = document.querySelector(
+    '.Catagory__Products__Container'
+  );
+  const catagoryPage = document.querySelector('.catagoryPage');
+  const catagoryTitle = document.querySelector('.catagory__title');
+
+  //! styles
+  resetDisplays();
+  catagoryPage.style.display = 'block';
+
+  //! Render
+  const filteredProducts = products.find(
+    (product) => product.catagory === gSelectedCatagory
+  );
+
+  catagoryTitle.innerText = filteredProducts.catagory;
+  const strHtml = filteredProducts.phones.map((phone) => {
+    return `<article class="product">
+    <img
+      src=${phone.imgUrl} />
+    <h1 class="product__title">${phone.model}</h1>
+    <h3>
+     ${phone.productInfo}
+    </h3>
+    <h2 class="product__price">
+      Price: <span class="product__price__value">${phone.price}</span>
+    </h2>
+    <button  onclick=renderProduct("${phone.id}")>Buy</button>
+  </article>`;
+  });
+
+  productContainer.innerHTML = strHtml.join(''); // join the array to string and swaps every , with a space
+}
+
+function renderProduct(model) {
+  //! Selectors
+  const itemPage = document.querySelector('.itemPage');
+  const itemPageContainer = document.querySelector('.itemPage__Container');
+  const catagoryPage = document.querySelector('.catagoryPage');
+
+  const productCatagory = products.find(
+    // finds the catagory of the product
+    (product) => product.catagory === gSelectedCatagory // every time takes 1 product from the array and returns it if the product.catagory is equal to the global selected catagory
+  );
+  const product = productCatagory.phones.find((phone) => phone.id === model); // finds the product in the array
+
+  const strHtml = ` 
+  <div class="itemPage__Border">
+
+  <div class="itemPage__Product__Container">
+    <div class="itemPage__productImage">
+      <img src=${product.imgUrl} alt="">
+    </div>
+    <div class="itemPage__Product__Info">
+      <h1 class="Product__Title">${product.model}</h1>
+      <div class="info">
+        <h3>Brand: <span>${gSelectedCatagory}</span></h3>
+        <h3>Product Code: <span>${makeid()}</span></h3>
+        <h3>Availability: <span>In Stock</span></h3>
+        <hr>
+        <h1 class="Info__Price">${product.price} $</h1>
+
+        <div class="actions">
+          <label for="qty">
+            QTY:
+            <div class="qty__container">
+
+              <input type="number" value="1" name="qty" min="1" max="5" />
+            </div>
+            
+          </label>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="itemPage__Description">
+  <header>Description</header>
+  <div>
+    <h3>Lorem, ipsum dolor.
+    </h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, illum!
+    </p>
+  </div>
+  <div>
+    <h3>Lorem, ipsum dolor.
+    </h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, illum!
+    </p>
+  </div>
+  <div>
+    <h3>Lorem, ipsum dolor.
+    </h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, illum!
+    </p>
+  </div>
+</div>
+  </div>`;
+  itemPageContainer.innerHTML = strHtml;
+
+  resetDisplays();
+  itemPage.style.display = 'block';
+}
+
+function resetDisplays() {
+  const itemPage = document.querySelector('.itemPage');
+  const catagoryPage = document.querySelector('.catagoryPage');
+
+  itemPage.style.display = 'none';
+  catagoryPage.style.display = 'none';
+}
--- products.js
+++ products.js
@@ -1,123 +1,179 @@
-let colors = [
-    "red",
-    "gray",
-    "blue",
-    "black"
-]
+const colors = ['red', 'gray', 'blue', 'black'];
 const products = [
-    {
-        catagory: 'apple',
-        phones: [{
-            model: "iphone 13",
-            price: 800,
-            colors,
-            productInfo: `Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode`,
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "iphone 13 pro max",
-            price: 1200,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "iphone 12",
-            price: 700,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "iphone 12 pro",
-            price: 850,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    },
-{
-        catagory: 'samsung',
-        phones: [{
-            model: "galaxy S21",
-            price: 600,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "galaxy S21 Plus",
-            price: 750,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "galaxy S22 ",
-            price: 800,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "galaxy S22 ultra",
-            price: 900,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    },
-{
-        catagory: 'xiaomi',
-        phones: [{
-            model: "redmi note10",
-            price: 600,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "redmi note10 pro",
-            price: 700,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "xiaomi 12",
-            price: 500,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "xiaomi 12 pro",
-            price: 580,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    },
-{
-        catagory: 'huawei',
-        phones: [{
-            model: "p30",
-            price: 400,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.medium.jpg'
-        }, {
-            model: "p30 pro",
-            price: 500,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://ercell.co.il/wp-content/uploads/2021/10/iphone-13-pro-max-gold-select-3.png'
-            
-        }, {
-            model: "p40",
-            price: 600,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://www.smilephone.co.il/stores/20266/n/16350262100-1.png'
-        }, {
-            model: "p40 pro",
-            price: 750,
-            colors,
-            productInfo: 'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
-            imgUrl:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHLN3?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1601352338000'
-        }]
-    }]
+  {
+    catagory: 'apple',
+    phones: [
+      {
+        id: 'iphone13',
+        model: 'iphone 13',
+        price: 800,
+        colors,
+        productInfo: 
+          `Apple's iPhone 13 features a ceramic shield front, Super Retina XDR display with True Tone and an A15 Bionic chip. The first design change users will notice is the smaller notch`,
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/173268.jpg?v=3776',
+      },
+      {
+        id: 'iphone13ProMax',
+        model: 'iphone 13 pro max',
+        price: 1200,
+        colors,
+        productInfo:
+          'Apple biggest phone in the lineup with a massive, 6.7" screen that for the first time in an iPhone comes with 120Hz ProMotion display that ensures super smooth scrolling',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/198271.jpg?v=3776',
+      },
+      {
+        id: 'iphone12',
+        model: 'iphone 12',
+        price: 700,
+        colors,
+        productInfo:
+          'The phone come in 6.1-inch and 5.4-inch sizes with identical features, including support for 5G cellular networks',
+        imgUrl: 'https://ksp.co.il/shop/items/512/124953.jpg?v=3776',
+      },
+      {
+        id: 'iphone12Pro',
+        model: 'iphone 12 pro',
+        price: 850,
+        colors,
+        productInfo:
+          'Best iPhone ever features the powerful A14 Bionic, all-new design with Ceramic Shield, pro camera system, LiDAR Scanner, and the biggest Super Retina XDR display ever on an iPhone',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/125095.jpg?v=3776',
+      },
+    ],
+  },
+  {
+    catagory: 'samsung',
+    phones: [
+      {
+        id:'galaxys21',
+        model: 'galaxy S21',
+        price: 600,
+        colors,
+        productInfo: 
+          'The Samsung Galaxy S21 specs are top-notch including a Snapdragon 888 chipset, 5G capability, 8GB RAM coupled with 128/256GB storage, and a 4000mAh battery.',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/185094.jpg?v=3776',
+      },
+      {
+        id:'galaxys21Plus',
+        model: 'galaxy S21 Plus',
+        price: 750,
+        colors,
+        productInfo:
+          'the phone come with 6.8" edge Quad HD+ Dynamic AMOLED 2X Infinity-O Display (3200x1440) 515 ppi HDR10+ certified 120Hz refresh rate.',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/191480.jpg?v=3776',
+      },
+      {
+        id:'galaxys22',
+        model: 'galaxy S22 ',
+        price: 800,
+        colors,
+        productInfo:
+          'top-notch including a Snapdragon 8 Gen 1 chipset, 8GB RAM coupled with 128/256GB storage, and a 3700mAh battery with 25W charging speed',
+        imgUrl: 'https://ksp.co.il/shop/items/512/191476.jpg?v=3776',
+      },
+      {
+        id:'galaxys22Ultra',
+        model: 'galaxy S22 ultra',
+        price: 900,
+        colors,
+        productInfo:
+          'the phone come with 6.8" edge Quad HD+ Dynamic AMOLED 2X Infinity-O Display (3088x1440) 500 ppi HDR10+ certified 120Hz refresh rate.',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/191449.jpg?v=3776',
+      },
+    ],
+  },
+  {
+    catagory: 'xiaomi',
+    phones: [
+      {
+        id:'redmiNote10',
+        model: 'redmi note10',
+        price: 600,
+        colors,
+        productInfo:
+          '  Rear Camera. 48MP Quad Camera Array · Front Camera. 13MP In-display Front Camera · Processor. Qualcomm® Snapdragon™ 678 · Operating system. MIUI 12 .',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/160103.jpg?v=3776',
+      },
+      {
+        id:'redmiNote10Pro',
+        model: 'redmi note10 pro',
+        price: 700,
+        colors,
+        productInfo:
+          'Processor Qualcomm Snapdragon 680 ; Front Camera 13MP ; Rear Camera 50MP + 8MP + 2MP + 2MP ; RAM 4GB, 6GB ; Storage 64GB, 128GB.',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/166282.jpg?v=3776',
+      },
+      {
+        id:'xiaomi12',
+        model: 'xiaomi 12',
+        price: 500,
+        colors,
+        productInfo:
+          'Resolution, 1080 x 2400 pixels, 20:9 ratio (~419 ppi density) ; Protection, Corning Gorilla Glass Victus ; Platform, OS ; Chipset, Qualcomm SM8450.',
+        imgUrl: 'https://ksp.co.il/shop/items/512/199066.jpg?v=3776',
+      },
+      {
+        id:'xiaomi12Pro',
+        model: 'xiaomi 12 pro',
+        price: 580,
+        colors,
+        productInfo:
+          'comes with a pro-grade 50MP triple camera array, WQHD+ dynamic 120Hz display, the most advanced Snapdragon® 8 Gen 1 chipset yet.',
+        imgUrl:
+          'https://ksp.co.il/shop/items/512/199059.jpg?v=3776',
+      },
+    ],
+  },
+  {
+    catagory: 'huawei',
+    phones: [
+      {
+        id:'p30',
+        model: 'p30',
+        price: 400,
+        colors,
+        productInfo:
+          'Featuring a sleek and durable design, an advanced new dual-camera system for improved photos and videos in low light, and introducing Cinematic mode',
+        imgUrl:
+          'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/plp/phones/new-phones/series-products/p30-pro-blue.png',
+      },
+      {
+        id:'p30Pro',
+        model: 'p30 pro',
+        price: 500,
+        colors,
+        productInfo:
+          'is powered by a 1.8GHz octa-core HiSilicon Kirin 980 processor that features 2 cores clocked at 2.6GHz, It comes with 6GB of RAM.',
+        imgUrl:
+          'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/plp/phones/new-phones/series-products/P30_orange.png',
+      },
+      {
+        id:'p40',
+        model: 'p40',
+        price: 600,
+        colors,
+        productInfo:
+          ' 6 GB and 8 GB RAM with the internal storage is 128 GB and 256 GB options. The Huawei P40 is fueled with a non-removable Li-Po 3800 mAh battery + Fast battery charging 22.5W.',
+        imgUrl: 'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/plp/phones/new-phones/series-products/p40-silver.png',
+      },
+      {
+        id:'p40Pro',
+        model: 'p40 pro',
+        price: 750,
+        colors,
+        productInfo:
+          '8 GB RAM and 128 GB/256GB/512GB internal storage options. The smartphone is powered by the HiSilicon Kirin 990 5G Octa-core processor.',
+        imgUrl:
+          'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/plp/phones/new-phones/series-products/p40-pro-plus-white.png',
+      },
+    ],
+  },
+];
